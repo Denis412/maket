8f225771839d3df18f4a986d7ea8e4f52ff4838a
Denis412 <d-a-malyshev@yandex,ru>
2023-02-16 21:12:12 +0300

    maket-v.3

--- src/components/UI/Main/Main.jsx
+++ src/components/UI/Main/Main.jsx
@@ -1,17 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Menu from "../Menu/Menu";
 
 import cl from "../Menu/Menu.module.css"
 import classes from "./Main.module.css"
 import MainSiteContent from "../MainSiteContent/MainSiteContent";
+import Fog from "../Fog/Fog";
+import MediaQuery, {useMediaQuery} from "react-responsive";
 
 const Main = () => {
-    const [hiddenMenu, setHiddenMenu] = useState(false);
+    const [hiddenMenu, setHiddenMenu] = useState(true);
+
+    const isDesktop = useMediaQuery({
+        query: "(min-width: 769px)",
+    });
+    const isTablet = useMediaQuery({
+        query: "(max-width: 768px)",
+    });
+
+    useEffect(() => {
+        isDesktop && setHiddenMenu(false);
+        isTablet && setHiddenMenu(true);
+    }, [isDesktop, isTablet]);
 
     return (
         <div className={classes.fullPage}>
-            <Menu hiddenMenu={hiddenMenu} setHiddenMenu={setHiddenMenu}/>
-            <MainSiteContent hiddenMenu={hiddenMenu}/>
+            <MediaQuery minWidth={769}>
+                <Menu hiddenMenu={hiddenMenu} setHiddenMenu={setHiddenMenu}/>
+            </MediaQuery>
+
+            <MainSiteContent hiddenMenu={hiddenMenu} setHiddenMenu={setHiddenMenu}/>
+
+            <MediaQuery maxWidth={768}>
+                <Fog visible={hiddenMenu} onClick={setHiddenMenu}/>
+                <Menu hiddenMenu={hiddenMenu} setHiddenMenu={setHiddenMenu}/>
+            </MediaQuery>
         </div>
     );
 };
